2c8b91ab94f801d3f7109186b7f42a3222b1c2c4
AshielBnt <samantha90p0@gmail.com>
2024-05-28 09:48:10 -0600

    condicionales version 1

---
+++ src/condicional.kt
@@ -0,0 +1,74 @@
+/*
+fun main(Array<String>) {
+    println("ingrese su sueldo")
+    val sueldo = readln().toDouble()
+    if(sueldo > 3000){
+        println("debe pagar impuestos")
+    }
+
+}
+
+ */
+/*
+fun main(Array<String >) {
+    print("ingrese el primer valor")
+    val valor 1 = readln().toLnt()
+    print("ingrese un segundo valor")
+    val valor 2 = readln().toLnt()
+    if (valor 1 > valor 2)
+        print("el mayor valor es $valor1")
+    else
+        print("el mayor valor es $valor2")
+
+
+ */
+/*
+}
+ fun main(parametro: Array<String>){
+     print("ingrese el primer valor:")
+     val valor1 = reandln().toLnt()
+     print("ingrese el segundo valor")
+     val valor2 = reandln().toLnt()
+     if (valor1 < valor2 ){
+         val suma = valor1 + valor2
+         val resta = valor1 - valor2
+         println("la suma de los dos valores es $suma")
+         println("la resta de los dos valores es $resta")
+         else{
+             val producto = valor1 * valor2
+             val division = valor1 / valor2
+             println("el producto de los dos valores es $producto")
+             println("la division de los dos productos es $division")
+         }
+     }
+ }
+
+ */
+/*
+
+fun main() {
+    // Solicitar al usuario que ingrese las tres notas
+    println("Ingrese la primera nota:")
+    val nota1 = readLine()!!.toDouble()
+
+    println("Ingrese la segunda nota:")
+    val nota2 = readLine()!!.toDouble()
+
+    println("Ingrese la tercera nota:")
+    val nota3 = readLine()!!.toDouble()
+
+    // Calcular el promedio
+    val promedio = (nota1 + nota2 + nota3) / 3
+
+    // Verificar si el promedio es mayor o igual a siete
+    if (promedio >= 7) {
+        println("¡Promocionado!")
+    } else {
+        println("Lo siento, no ha alcanzado el promedio mínimo para ser promocionado.")
+    }
+}
+
+
+
+ */
+
